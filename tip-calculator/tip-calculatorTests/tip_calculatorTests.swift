2d1bf0c02520c1b32c07d7fbecda98aa11dd9c9b
Rostyslav Didenko <didenko.rostyslav@depositphotos.com>
2023-08-21 12:11:43 +0300

    finish

--- tip-calculator/tip-calculatorTests/tip_calculatorTests.swift
+++ tip-calculator/tip-calculatorTests/tip_calculatorTests.swift
@@ -27,14 +27,60 @@ final class tip_calculatorTests: XCTestCase {
         cancellables = nil
     }
     
-    func testResultWithoutTipFor1Person() {
-
-        let input = buildInput()
+    func testResultWithoutTipFor2Person() {
+        let bill: Double = 100.0
+        let tip: Tip = .none
+        let split: Int = 2
+        
+        let input = buildInput(
+            bill: bill,
+            tip: tip,
+            split: split
+        )
+        
         let output = sut.transform(input: input)
         output.updateViewPublisher.sink { result in
-            XCTAssertEqual(result.amountPerPerson, 100)
+            XCTAssertEqual(result.amountPerPerson, 50)
             XCTAssertEqual(result.totalBill, 100)
             XCTAssertEqual(result.totalTip, 0)
+        }.store(in: &cancellables)
+    }
+    
+    func testResultWith10PercentTipFor2Person() {
+        let bill: Double = 100.0
+        let tip: Tip = .tenPercent
+        let split: Int = 2
+        
+        let input = buildInput(
+            bill: bill,
+            tip: tip,
+            split: split
+        )
+        
+        let output = sut.transform(input: input)
+        output.updateViewPublisher.sink { result in
+            XCTAssertEqual(result.amountPerPerson, 55)
+            XCTAssertEqual(result.totalBill, 110)
+            XCTAssertEqual(result.totalTip, 10)
+        }.store(in: &cancellables)
+    }
+    
+    func testResultWithCustomTipFor4Person() {
+        let bill: Double = 100.0
+        let tip: Tip = .twentyPercent
+        let split: Int = 4
+        
+        let input = buildInput(
+            bill: bill,
+            tip: tip,
+            split: split
+        )
+        
+        let output = sut.transform(input: input)
+        output.updateViewPublisher.sink { result in
+            XCTAssertEqual(result.amountPerPerson, 30)
+            XCTAssertEqual(result.totalBill, 120)
+            XCTAssertEqual(result.totalTip, 20)
         }.store(in: &cancellables)
     }
     
